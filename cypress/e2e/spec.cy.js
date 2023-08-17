b70be56eca58d06c07452f74ef486e97768b26b6
Leit <hanwi1994@gmail.com>
2023-08-17 16:24:33 +0200

    Solved using jQuery selector

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,9 +11,29 @@ it('clicks the Next button until we get to the last page', () => {
   // can you click the "Next" button until
   // we get to the very last page?
   // button selector "[value=next]"
-  cy.get('[value=next]') // keep clicking!
+  cy.get('[value=next]').then(($nextbutton) => {
+    while (!$nextbutton.is(':disabled')) {
+      $nextbutton.click()
+    }
+  }) // keep clicking!
 
   cy.log('**confirm we are on the last page**')
   cy.get('[value=next]').should('be.disabled')
   cy.get('[value=last]').should('be.disabled')
 })
+
+// :disabled selector
+// Description: Selects all elements that are disabled.
+// As with other pseudo-class selectors (those that begin with a ":"), it is recommended
+// to precede it with a tag name or some other selector; otherwise, the universal
+// selector ("*") is implied. In other words, the bare $(':disabled') is equivalent
+// to $('*:disabled'), so $('input:disabled') or similar should be used instead.
+
+// Although their resulting selections are usually the same, the :disabled selector
+// is subtly different from the [disabled] attribute selector;:disabled matches elements 
+// that are actually disabled while [disabled] only checks for the existence of the disabled attribute.
+
+// The :disabled selector should only be used for selecting HTML elements that support 
+// the disabled attribute (<button>, <input>, <optgroup>, <option>, <select>, <textarea>, 
+//   <menuitem>, and <fieldset>).
+
